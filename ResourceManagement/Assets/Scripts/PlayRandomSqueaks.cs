641ba14cc535ad8b5fdf0c7770e20369fcd527be
Jaden Goter <jadengoter@gmail.com>
2024-06-24 22:14:13 -0700

    More sfx

---
+++ ResourceManagement/Assets/Scripts/PlayRandomSqueaks.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayRandomSqueaks : MonoBehaviour
+{
+    [SerializeField] private GameObject squeakSfx;
+    [SerializeField] private float avgTimeBetweenSqueaks;
+    [SerializeField] private float varianceBetweenSqueaks;
+
+    private void Awake()
+    {
+        StartCoroutine(Squeak());
+    }
+
+    private IEnumerator Squeak()
+    {
+        while (true)
+        {
+            Instantiate(squeakSfx, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(avgTimeBetweenSqueaks + Random.Range(0, varianceBetweenSqueaks));
+        }
+    }
+}
--- ResourceManagement/Assets/Scripts/Presentation/GameEventQueues.cs
+++ ResourceManagement/Assets/Scripts/Presentation/GameEventQueues.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using Simulation;
 using UnityEngine;
@@ -27,12 +28,54 @@ namespace Presentation
         public Queue<RatThrown> RatsThrown;
         //public Queue<> ;
 
+        [SerializeField] private GameObject scoreRatSfx;
+        [SerializeField] private GameObject ratPickupSfx;
+        [SerializeField] private GameObject ratThrowSfx;
+
+        [SerializeField] private bool testEvents;
+
         void Start()
         {
             Instance = this;
             RatsScored = new Queue<PendingRatScored>();
             RatsPickedUp = new Queue<RatPickedUp>();
             RatsThrown = new Queue<RatThrown>();
+
+            if (testEvents)
+                StartCoroutine(TestEvents());
+        }
+        
+        private void Update()
+        {
+            while (RatsScored.Count > 0)
+            {
+                PendingRatScored score = RatsScored.Dequeue();
+                Instantiate(scoreRatSfx, score.CauldronSplashCenter, Quaternion.identity);
+            }
+
+            while (RatsPickedUp.Count > 0)
+            {
+                RatPickedUp pickup = RatsPickedUp.Dequeue();
+                Instantiate(ratPickupSfx, pickup.RatPosition, Quaternion.identity);
+            }
+
+            while (RatsThrown.Count > 0)
+            {
+                RatThrown thrownRat = RatsThrown.Dequeue();
+                Instantiate(ratThrowSfx, thrownRat.Position, Quaternion.identity);
+            }
+        }
+
+        private IEnumerator TestEvents()
+        {
+            yield return new WaitForSeconds(3);
+            RatsPickedUp.Enqueue(new RatPickedUp());
+
+            yield return new WaitForSeconds(3);
+            RatsThrown.Enqueue(new RatThrown());
+
+            yield return new WaitForSeconds(3);
+            RatsScored.Enqueue(new PendingRatScored());
         }
     }
 }
